830f0112025c1206d582fa473579f39d8cb71586
Aryan Curiel Pardo <aryan.curiel@gmail.com>
2020-10-16 10:01:12 -0700

    removed debug log

--- profiles/validators.py
+++ profiles/validators.py
@@ -2,6 +2,5 @@ from django.core.exceptions import ValidationError
 from datetime import date
 
 def is_past_date(value):
-    print(value)
     if value > date.today():
         raise ValidationError(f'{value} is a future date')
