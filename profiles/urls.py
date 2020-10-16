e63c8d00ece704a84b35a293ff609d5426e528bb
Aryan Curiel Pardo <aryan.curiel@gmail.com>
2020-10-16 06:39:07 -0700

    fixed app and urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from profiles.views import *
 
 router = DefaultRouter()
-# router.register(r'users', UserViewSet, basename='user')
+router.register(r'', UserProfileViewSet, basename='profile')
 urlpatterns = router.urls
