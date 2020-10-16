bf9580e148eb412c96462f189ca5ec6091bdb835
Aryan Curiel Pardo <aryan.curiel@gmail.com>
2020-10-16 06:45:08 -0700

    Fixed wrong method call

--- profiles/validators.py
+++ profiles/validators.py
@@ -2,7 +2,8 @@ from django.core.exceptions import ValidationError
 from datetime import date
 
 def is_past_date(value):
-    if not value < date.today:
+    print(value)
+    if not value < date.today():
         raise ValidationError(
             _('%(value)s is a future date'),
             params={'value': value},
